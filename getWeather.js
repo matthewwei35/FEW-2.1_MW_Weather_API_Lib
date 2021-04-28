69144ab6fcb75feb0edd2679f37135aba3a75a36
Matthew Wei <matthewwei35@gmail.com>
2021-04-28 13:26:18 -0700

    Initial commit

---
+++ getWeather.js
@@ -0,0 +1,26 @@
+async function getWeather(path) {
+  const res = await fetch(path)
+  const json = await res.json()
+  return {
+    name: json.name,
+    temp: json.main.temp,
+    desc: json.weather[0].description
+  }
+}
+
+function getWeatherByZip(zip, apiKey, units) {
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+  return getWeather(path)
+}
+
+function getWeatherByCity(city, apiKey, units) {
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${city}&appid=${apiKey}&units=${units}`
+  return getWeather(path)
+}
+
+function getWeatherByGeo(lat, lon, apiKey, units) {
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+  return getWeather(path)
+}
+
+export { getWeatherByZip, getWeatherByCity, getWeatherByGeo }
