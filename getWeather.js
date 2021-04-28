03ed9d0c68bbe3d0bd2681cd9bfaa788c6e1d358
Matthew Wei <matthewwei35@gmail.com>
2021-04-28 14:37:42 -0700

    style(index/getweather): linted js

--- getWeather.js
+++ getWeather.js
@@ -1,26 +1,26 @@
 async function getWeather(path) {
-  const res = await fetch(path)
-  const json = await res.json()
+  const res = await fetch(path);
+  const json = await res.json();
   return {
     name: json.name,
     temp: json.main.temp,
-    desc: json.weather[0].description
-  }
+    desc: json.weather[0].description,
+  };
 }
 
 function getWeatherByZip(zip, apiKey, units) {
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  return getWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  return getWeather(path);
 }
 
 function getWeatherByCity(city, apiKey, units) {
-  const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
-  return getWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  return getWeather(path);
 }
 
 function getWeatherByGeo(lat, lon, apiKey, units) {
-  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
-  return getWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  return getWeather(path);
 }
 
-export { getWeatherByZip, getWeatherByCity, getWeatherByGeo }
+export { getWeatherByZip, getWeatherByCity, getWeatherByGeo };
